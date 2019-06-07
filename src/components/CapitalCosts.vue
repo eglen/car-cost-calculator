23a263ee894c8075ff8da105837a52e6675f986b
Edward Glen <eglen.git@gmail.com>
2019-06-06 22:21:42 -0700

    Basic depreciation with vue-cli infrastructure

---
+++ src/components/CapitalCosts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="number" v-model="purchasePrice" />
+    <p>{{ monthlyCosts }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CapitalCosts",
+
+  data: function() {
+    return {
+      purchasePrice: 456463
+    };
+  },
+  computed: {
+    monthlyCosts: function() {
+      var monthlyDepreciation = [];
+      for (var month = 0; month < 120; month++) {
+        monthlyDepreciation.push(this.costForMonth(this.purchasePrice, month));
+      }
+      return monthlyDepreciation;
+    }
+  },
+  methods: {
+    costForMonth(costNew, month) {
+      var ratio = -0.0125;
+      return costNew * Math.exp(ratio * month);
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+h1 {
+  margin: 40px 0 0;
+}
+</style>
